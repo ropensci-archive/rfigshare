4aa2d47f3b7f48e7412b5539c4d02cc96c5f129a
Carl Boettiger <cboettig@gmail.com>
2012-08-29 17:59:46 -0700

    PUT & POST methods still not working

--- R/addauthors.R
+++ R/addauthors.R
@@ -1,14 +1,16 @@
 #' Add authors to article
+#' @param author_id the id number of a registered figshare user (see figshare_search_authors)
+#' @param id number of an article on figshare 
+#' @param the session authentication token from figshare_auth()
+#' @return adds the requested author to the given article
+#' @export
+#' #import RJSONIO
 
-HTTP method PUT
-PATH  /my_data/articles/{article_id}/authors
-Parameters  author_id
-Content-type  application/json
 
 figshare_addauthors <- function(author_id, article_id, session){
-  base <- "http://api.figshare.com/v1/"
-  method <- paste("/my_data/articles", article_id, "authors", sep= "/")
-  request = paste(base, method, sep="")
+  base <- "http://api.figshare.com/v1"
+  method <- paste("my_data/articles", article_id, "authors", sep= "/")
+  request = paste(base, method, sep="/")
   body <- toJSON(list("author_id"=author_id, "Content-Type" = "application/json"))
   out <- PUT(request, config=session, body=body)
 }
--- R/createarticle.R
+++ R/createarticle.R
@@ -5,11 +5,12 @@
 figshare_create <- 
 function(title="test", description="description of test", type="dataset", session){
   require(RJSONIO)
-  base <- "http://api.figshare.com/v1/"
-  method <- paste("/my_data/articles")
-  request <- paste(base, method, sep="")
+  base <- "http://api.figshare.com/v1"
+  method <- paste("my_data/articles")
+  request <- paste(base, method, sep="/")
   meta <- toJSON(list("title"=title, "description"=description,
                       "type"=type, "Content-Type" = "application/json"))
+
   POST(request, config=session, body=meta)
 }
 
