8d3d7da6ac6b49b7d5b80c67bf54630e8ba5c1b0
Carl Boettiger <cboettig@gmail.com>
2012-08-30 09:56:29 -0700

    debugging authentication

--- R/createarticle.R
+++ R/createarticle.R
@@ -2,6 +2,12 @@
 # author: Carl Boettiger
 # date: 28 June 2012
 
+#' Create a FigShare article
+#' @param title for the article
+#' @param description of the article
+#' @param type one of dataset, figure, media, poster, or paper
+#' @import RJSONIO
+#' @export
 figshare_create <- 
 function(title="test", description="description of test", type="dataset", session){
   require(RJSONIO)
@@ -9,8 +15,8 @@ function(title="test", description="description of test", type="dataset", sessio
   method <- paste("my_data/articles")
   request <- paste(base, method, sep="/")
   meta <- toJSON(list("title"=title, "description"=description,
-                      "type"=type, "Content-Type" = "application/json"))
-  POST(request, config=session, body=meta)
+                      "type"=type))
+  POST(request, config=session, body=meta, add_headers("Content-Type" = "application/json"))
 }
 
 
