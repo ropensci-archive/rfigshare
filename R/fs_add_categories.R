9d9a9134b8fc24cc756108241dd4b79da5e8a329
Carl Boettiger <cboettig@gmail.com>
2012-09-07 15:43:31 -0700

    vectorize categories

--- R/fs_add_categories.R
+++ R/fs_add_categories.R
@@ -1,4 +1,25 @@
-#' Add category to article
+#' Add categories to article
+#' 
+#' @author Carl Boettiger \email{cboettig@@gmail.com}
+#' @param article_id the id number of the article 
+#' @param categories a list of categories.  Either the category names or numeric codes must be supplied, see \code{\link{fs_show_categories}})
+#' @param session (optional) the authentication credentials from \code{\link{fs_auth}}. If not provided, will attempt to load from cache as long as figshare_auth has been run.  
+#' @return output of PUT request (invisibly)
+#' @seealso \code{\link{fs_auth}}
+#' @references \url{http://api.figshare.com}
+#' @import RJSONIO httr
+#' @export
+#' @examples \dontrun{
+#' fs_auth()
+#' fs_category(138, 10) 
+#' }
+fs_add_categories <- 
+function(article_id, categories, session = fs_get_auth()){
+  sapply(categories, function(category) fs_add_category(article_id, category, session)
+}
+
+
+#' Add a category to article
 #' 
 #' @author Carl Boettiger \email{cboettig@@gmail.com}
 #' @param article_id the id number of the article 
--- R/fs_add_tags.R
+++ R/fs_add_tags.R
@@ -13,6 +13,26 @@
 #'  fs_auth()
 #'  fs_add_tag(138, "phylogenetics") 
 #' }
+fs_add_tags <- function(article_id, tags, session=fs_get_auth()){
+  sapply(tags, function(tag) fs_add_tag(article_id, tag, session=session))
+}
+
+
+#' Add a tag to an article
+#' 
+#' @author Carl Boettiger \email{cboettig@@gmail.com}
+#' @param article_id the id number of the article to create
+#' @param tag name of the tag to add
+#' @param session the authentication credentials from \code{\link{fs_auth}}
+#' @return output of PUT request (invisibly)
+#' @seealso \code{\link{fs_auth}}
+#' @references \url{http://api.figshare.com}
+#' @import RJSONIO
+#' @keywords internal
+#' @examples \dontrun{
+#'  fs_auth()
+#'  fs_add_tag(138, "phylogenetics") 
+#' }
 fs_add_tag <- 
 function(article_id, tag, session = fs_get_auth()){
   base <- "http://api.figshare.com/v1"
@@ -24,6 +44,5 @@ function(article_id, tag, session = fs_get_auth()){
    post <- PUT(request, config=config, body=body)
   invisible(post)
 }
-
 
 
