fd14fced1bb73fb4d530cd96ab7b7a3d1c4bad57
Edmund Hart <edmund.m.hart@gmail.com>
2012-09-09 22:33:36 -0700

    Addressed issues 9 and 11

--- R/fs_add_categories.R
+++ R/fs_add_categories.R
@@ -1,29 +1,8 @@
-#' Add categories to article
-#' 
-#' @author Carl Boettiger \email{cboettig@@gmail.com}
-#' @param article_id the id number of the article 
-#' @param categories a list of categories.  Either the category names or numeric codes must be supplied, see \code{\link{fs_show_categories}})
-#' @param session (optional) the authentication credentials from \code{\link{fs_auth}}. If not provided, will attempt to load from cache as long as figshare_auth has been run.  
-#' @return output of PUT request (invisibly)
-#' @seealso \code{\link{fs_auth}}
-#' @references \url{http://api.figshare.com}
-#' @import RJSONIO httr
-#' @export
-#' @examples \dontrun{
-#' fs_auth()
-#' fs_category(138, 10) 
-#' }
-fs_add_categories <- 
-function(article_id, categories, session = fs_get_auth()){
-  sapply(categories, function(category) fs_add_category(article_id, category, session))
-}
-
-
 #' Add a category to article
 #' 
 #' @author Carl Boettiger \email{cboettig@@gmail.com}
 #' @param article_id the id number of the article 
-#' @param category_id Must be a category id number.  Support for actual category names coming soon.  
+#' @param category_id is a vector of integers corresponding to categories or a vector of category names  
 #' @param session (optional) the authentication credentials from \code{\link{fs_auth}}. If not provided, will attempt to load from cache as long as figshare_auth has been run.  
 #' @return output of PUT request (invisibly)
 #' @seealso \code{\link{fs_auth}}
@@ -32,26 +11,47 @@ function(article_id, categories, session = fs_get_auth()){
 #' @export
 #' @examples \dontrun{
 #' fs_auth()
-#' fs_category(138, 10) 
 #' }
-fs_add_category <- 
-function(article_id, category_id, session = fs_get_auth()){
+fs_add_categories <- function(article_id, category_id, session = fs_get_auth()){
+  options(warn=-1)
+  my_ids <- as.numeric(category_id)
+  if(sum(is.na(my_ids)) > 0){category_id <- fs_cat_to_id(category_id)}
+  
   base <- "http://api.figshare.com/v1"
   method <- paste("my_data/articles", article_id, "categories", sep= "/")
   request <- paste(base, method, sep="/")
-  body <- toJSON(list("category_id"=category_id))
-  config <- c(verbose(), session, 
-              add_headers("Content-Type" = "application/json"))
   
   for(i in 1:length(category_id)){
     body <- toJSON(list("category_id"=category_id[i]))
     config <- c(verbose(), session, 
                 add_headers("Content-Type" = "application/json"))
     
-     post <- PUT(request, config=config, body=body)
-     invisible(post)
+    post <- PUT(request, config=config, body=body)
+    invisible(post)
   }
 }
 
 
 
+#'  Helper function that matches string categories to id's
+#' 
+#' @author Edmund Hart \email{edmundm.h.hart@gmail.com}
+#' @param category_id Must be a valid category string, regardless of case 
+#' @return a vector of integers corresponding to valid figshare categories
+#' @references \url{http://api.figshare.com}
+#' @import RJSONIO httr plyr
+#' @examples \dontrun{
+#' fs_auth()
+#' }
+
+fs_cat_to_id <- function(category_id){
+  if(!exists("cat_names")) cat_names <<- content(GET("http://api.figshare.com/v1/categories"),as="parsed")
+  name_db <- ldply(cat_names$items, data.frame)  
+  name_db$name <- tolower(name_db$name)
+  my_matches <- match(tolower(category_id),name_db$name)
+  
+  if( sum(is.na(my_matches)) > 0) {stop("One or more of your input strings is not a valid category") }
+  
+  if( sum(is.na(my_matches)) == 0 ) {return(name_db$id[my_matches]) }
+  
+}
--- R/fs_add_tags.R
+++ R/fs_add_tags.R
@@ -1,23 +1,3 @@
-#' Add tags to article
-#' 
-#' @author Carl Boettiger \email{cboettig@@gmail.com}
-#' @param article_id the id number of the article to create
-#' @param tag name of the tag to add
-#' @param session the authentication credentials from \code{\link{fs_auth}}
-#' @return output of PUT request (invisibly)
-#' @seealso \code{\link{fs_auth}}
-#' @references \url{http://api.figshare.com}
-#' @import RJSONIO
-#' @export
-#' @examples \dontrun{
-#'  fs_auth()
-#'  fs_add_tag(138, "phylogenetics") 
-#' }
-fs_add_tags <- function(article_id, tags, session=fs_get_auth()){
-  sapply(tags, function(tag) fs_add_tag(article_id, tag, session=session))
-}
-
-
 #' Add a tag to an article
 #' 
 #' @author Carl Boettiger \email{cboettig@@gmail.com}
@@ -28,24 +8,24 @@ fs_add_tags <- function(article_id, tags, session=fs_get_auth()){
 #' @seealso \code{\link{fs_auth}}
 #' @references \url{http://api.figshare.com}
 #' @import RJSONIO
+#' @export
 #' @keywords internal
 #' @examples \dontrun{
 #'  fs_auth()
 #'  fs_add_tag(138, "phylogenetics") 
 #' }
-fs_add_tag <- 
-function(article_id, tag, session = fs_get_auth()){
-  base <- "http://api.figshare.com/v1"
-  method <- paste("my_data/articles", article_id, "tags", sep= "/")
-  request <- paste(base, method, sep="/")
-  
-  for(i in 1:length(tag)){
-   body <- toJSON(list("tag_name" = tag[i]))
-   config <- c(verbose(), session, 
-              add_headers("Content-Type" = "application/json"))
-    post <- PUT(request, config=config, body=body)
-   invisible(post)
+fs_add_tags <- 
+  function(article_id, tag, session = fs_get_auth()){
+    base <- "http://api.figshare.com/v1"
+    method <- paste("my_data/articles", article_id, "tags", sep= "/")
+    request <- paste(base, method, sep="/")
+    for(i in 1:length(tag)){
+      body <- toJSON(list("tag_name" = tag[i]))
+      config <- c(verbose(), session, 
+                  add_headers("Content-Type" = "application/json"))
+      
+      post <- PUT(request, config=config, body=body)
+      invisible(post)
+      
+    }
   }
-}
-
-
