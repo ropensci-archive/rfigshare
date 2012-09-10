a06affd84ccf190b6303c883c9f2f44b39751867
Edmund Hart <edmund.m.hart@gmail.com>
2012-09-10 15:55:57 -0700

    Removed the extra /
    Per Hadley's suggestion.

--- R/fs_upload.R
+++ R/fs_upload.R
@@ -9,7 +9,7 @@
 #' @references \url{http://api.figshare.com}
 #' @export
 fs_upload <- function(article_id, file, session = fs_get_auth()){
-  base <- "http://api.figshare.com/v1/"
+  base <- "http://api.figshare.com/v1"
   method <- paste("/my_data/articles", article_id, "files", sep= "/")
   request = paste(base, method, sep="")
   body = toJSON(list(filedata = upload_file(file)))
