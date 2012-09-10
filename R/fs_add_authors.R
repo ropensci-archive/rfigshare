5ec49a64f258e4405f10dc5ab7c028032356742c
Carl Boettiger <cboettig@gmail.com>
2012-09-09 17:31:57 -0700

    Documentation

--- R/fs_add_authors.R
+++ R/fs_add_authors.R
@@ -4,12 +4,11 @@
 #' @param article_id id number of an article on figshare 
 #' @param author_id the id number of a registered figshare user (see \code{\link{fs_author_search}})
 #' @param session (optional) the authentication credentials from \code{\link{fs_auth}}. If not provided, will attempt to load from cache as long as figshare_auth has been run.  
-#' @return adds the requested author to the given article
-#' @import RJSONIO
+#' @return adds the requested authors to the given article
 #' @export
 #' @examples \dontrun{
 #'  fs_auth()
-#'  fs_add_author("123", "456")
+#'  fs_add_authors("138", list("Scott Chamberlain", "Karthik Ram"))
 #' } 
 fs_add_authors  <- function(article_id, authors, 
                             session = fs_get_auth(), create_missing=FALSE){
@@ -35,10 +34,15 @@ fs_add_authors  <- function(article_id, authors,
 }
 
 
-#' fs_author_ids
+#' Get Author IDs from names
+#' 
 #' Take an author list, search for each author and return their FigShare ID.  
 #' If no author is found, call fs_create_author and return the ID.
 #' If multiple matches are found, allow user to choose interactively
+#' @param authors a list/vector of authors (not a character string)
+#' @param graphics logical (default False) use graphic input to disambiguate?
+#' @param session (optional) the authentication credentials from \code{\link{fs_auth}}. If not provided, will attempt to load from cache. 
+#' @return a list of author id numbers, or NULLS where ids cannot be found.   
 fs_author_ids <- function(authors, session = fs_get_auth(), graphics=FALSE){
   authors_info <- lapply(authors, function(author){
                          matches <- fs_author_search(author, session)
@@ -68,7 +72,8 @@ fs_author_ids <- function(authors, session = fs_get_auth(), graphics=FALSE){
  
 #' @param article_id id number of an article on figshare 
 #' @param author_id the id number of a registered figshare user (see \code{\link{fs_author_search}})
-#' @param session (optional) the authentication credentials from \code{\link{fs_auth}}. If not provided, will attempt to load from cache as long as figshare_auth has been run.  
+#' @param session (optional) the authentication credentials from \code{\link{fs_auth}}. If not provided, will attempt to load from cache.  
+#' @import RJSONIO
 fs_add_author_id <- function(article_id, author_id, session = fs_get_auth()){
   base <- "http://api.figshare.com/v1"
   method <- paste("my_data/articles", article_id, "authors", sep= "/")
