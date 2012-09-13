218288e1ecf0c96beebdef5b71de7fd2b704aa49
Karthik Ram <karthik.ram@gmail.com>
2012-09-13 14:38:10 -0700

    added a startup function so people aren't clueless about what to do first.

---
+++ R/startup.R
@@ -0,0 +1,3 @@
+.onAttach <- function(...) {
+    packageStartupMessage("\n\n New to rfigshare? Look up ?fs_auth() for setting up an app and authenticating for all other functions. Use suppressPackageStartupMessages() to suppress this message in future\n")
+} 
