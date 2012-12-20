4b8c4f6b5c23ae71b37c3ba05e4148c1a474aa5e
Carl Boettiger <cboettig@gmail.com>
2012-12-19 21:11:47 -0800

    `fs_update` can now take a list of files #35
    If one article id is given and the created article is already of type
    'fileset', but more than one file is given, they will all be uploaded
    to that article id.
    
    If more than one article id is given, and a list of files is given,
    then each id will get the matching file in the list.
    
    See the documentation ?fs_upload for details.
    
    Method needs testing and test cases written.  should close #35

--- R/fs_upload.R
+++ R/fs_upload.R
@@ -2,9 +2,12 @@
 #' 
 #' @details Article must be a draft, i.e. created by \code{\link{fs_create}} and not yet made public or private. Only articles of type "fileset" can have multiple files uploaded.  
 #' @author Carl Boettiger \email{cboettig@@gmail.com}
-#' @param article_id number
-#' @param file path to file to upload
+#' @param article_id an article id number or a character string (or list) of numbers
+#' @param file path to file to upload, or character string (or list) of files (paths)
 #' @param session the authentication credentials from \code{\link{fs_auth}} (optional)
+#' @details If only a single id number is given but a character string of files is given,
+#' then be sure that the id corresponds to an object of type "fileset".  If article_id list
+#' has more than one id, then there must be a corresponding file path for each id.  
 #' @seealso \code{\link{fs_auth}}
 #' @references \url{http://api.figshare.com}
 #' @examples
@@ -14,6 +17,39 @@
 #' } 
 #' @export
 fs_upload <- function(article_id, file, session = fs_get_auth()){
+  # handle lists by converting to expected type.  
+  if(is(article_id, "list"))
+    article_id <- as.character(article_id)
+  if(is(file, "list"))
+    file <- as.character(file)
+
+  if(length(article_id) == 1 && length(file) == 1)
+    fs_upload_one(article_id, file, session)
+  if(length(article_id) > 1)
+    sapply(1:length(article_id), function(i) fs_upload_one(article_id[i], file[i], session))
+  if(length(article_id) == 1 && length(file) > 1)
+    sapply(file, function(f) fs_upload_one(article_id, f, session))
+}
+
+
+
+
+#'  Upload file to an article 
+#' 
+#' @details Article must be a draft, i.e. created by \code{\link{fs_create}} and not yet made public or private. Only articles of type "fileset" can have multiple files uploaded.  
+#' @author Carl Boettiger \email{cboettig@@gmail.com}
+#' @param article_id number
+#' @param file path to file to upload
+#' @param session the authentication credentials from \code{\link{fs_auth}} (optional)
+#' @seealso \code{\link{fs_auth}}
+#' @references \url{http://api.figshare.com}
+#' @keywords internal
+#' @examples
+#' \dontrun{
+#' id <- fs_create("Title", "description", "figure")
+#' fs_upload(id, "file.png")
+#' } 
+fs_upload_one  <- function(article_id, file, session = fs_get_auth()){
   base <- "http://api.figshare.com/v1"
   method <- paste("/my_data/articles", article_id, "files", sep= "/")
   request = paste(base, method, sep="")
