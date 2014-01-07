467c35ffac57277a77b681cdd339ecfb4f6b96ec
Carl Boettiger <cboettig@gmail.com>
2014-01-06 21:06:51 -0800

    enable yaml method

--- R/fs_object_methods.R
+++ R/fs_object_methods.R
@@ -1,15 +1,15 @@
 #' @import yaml
 #' @S3method print fs_object
 print.fs_object <- function(x, ...){
-#  as.yaml(x)
-x
+  as.yaml(x)
+# x
 }
 
 #' @import yaml
 #' @S3method summary fs_object
 summary.fs_object <- function(object, ...){
-#  as.yaml(object)
-x
+  as.yaml(object)
+# object
 }
 
 
