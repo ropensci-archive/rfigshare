01b39beeca5dc16f57689d4923af7bb08a8fce4c
Carl Boettiger <cboettig@gmail.com>
2014-03-20 15:59:20 -0700

    remove fs_embed and XML import, since function not working

--- R/fs_embed.R
+++ R/fs_embed.R
@@ -1,34 +1,34 @@
-
-#' get image url
-#' 
-#' @param id a figshare article id number 
-#' @return a url to the image file
-#' @details this is currently an unstable hack of html parsing
-#' @import XML httr
-fs_image_url <- function(id) {
-  a <- fs_details(id)
-  b <- GET(a$doi)
-  b_parsed <- content(b, as = "parsed", type="text/html")
-  img_url <- unlist(xmlToList(b_parsed$children$html))
-  imgs <- grep("\\.png", img_url)
-  m <- grep("figshare.com/media", img_url[imgs])
-  out <- img_url[imgs][m]
-  out
-}
-
-
-#' Upload a figure to figshare and return the url
-#' 
-#' @param file path to an image file
-#' @return a url to the image file
-#' @details use with opts_knit$set(upload.fn = fs_embed)
-#' @export
-fs_embed <- function(file) {
-  ## Read in title, and details from an environment?  from chunk? 
-  title <- file 
-  description <- "embedded file automatically uploaded to figshare from R"
-  id <- fs_new_article(title, description, type = "figure", visibility = "public")
-  fs_image_url(id)
-}
-
-
+#
+##' get image url
+##' 
+##' @param id a figshare article id number 
+##' @return a url to the image file
+##' @details this is currently an unstable hack of html parsing
+##' @import XML httr
+#fs_image_url <- function(id) {
+#  a <- fs_details(id)
+#  b <- GET(a$doi)
+#  b_parsed <- content(b, as = "parsed", type="text/html")
+#  img_url <- unlist(xmlToList(b_parsed$children$html))
+#  imgs <- grep("\\.png", img_url)
+#  m <- grep("figshare.com/media", img_url[imgs])
+#  out <- img_url[imgs][m]
+#  out
+#}
+#
+#
+##' Upload a figure to figshare and return the url
+##' 
+##' @param file path to an image file
+##' @return a url to the image file
+##' @details use with opts_knit$set(upload.fn = fs_embed)
+##' @export
+#fs_embed <- function(file) {
+#  ## Read in title, and details from an environment?  from chunk? 
+#  title <- file 
+#  description <- "embedded file automatically uploaded to figshare from R"
+#  id <- fs_new_article(title, description, type = "figure", visibility = "public")
+#  fs_image_url(id)
+#}
+#
+#
